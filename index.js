f50376daedc706de06c209a708c16920007489b7
Andrew A. Stenipanin <and5251@yandex.ru>
2020-08-18 02:33:10 +0300

    register and login users

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
 const { ApolloServer } = require("apollo-server");
-const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
 const Post = require("./models/Post");
 const { MONGODB } = require("./config");
 
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find()
-        return posts
-      } catch(err) {
-        throw new Error(err)
-      }
-    }
-  },
-};
 
 const server = new ApolloServer({
   typeDefs,
@@ -44,7 +21,3 @@ mongoose
   .then((res) => {
     console.log(`Server runnig at ${res.url}`);
   });
-
-// server.listen({ port: 5000 }).then((res) => {
-//   console.log(`Server runnig at ${res.url}`);
-// });
