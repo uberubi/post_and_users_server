e87e5de03c3e3aed49c77c2392ed1f828bf2547c
Andrew A. Stenipanin <and5251@yandex.ru>
2020-08-19 03:51:43 +0300

    likes, comments, subscription

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,58 +1,89 @@
-const Post = require('../../models/Post')
-const checkAuth = require('../../utils/check-auth')
-const { AuthenticationError } = require("apollo-server");
+const Post = require("../../models/Post");
+const checkAuth = require("../../utils/check-auth");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find().sort({createdAt: -1})
-        return posts
-      } catch(err) {
-        throw new Error(err)
+        const posts = await Post.find().sort({ createdAt: -1 });
+        return posts;
+      } catch (err) {
+        throw new Error(err);
       }
     },
-    async getPost(_, {postId}) {
+    async getPost(_, { postId }) {
       try {
-        const post = await Post.findById(postId)
-        if(post) {
-          return post
+        const post = await Post.findById(postId);
+        if (post) {
+          return post;
         } else {
-          throw new Error('Post not found')
+          throw new Error("Post not found");
         }
-      } catch(err) {
-        throw new Error(err)
+      } catch (err) {
+        throw new Error(err);
       }
-    }
+    },
   },
   Mutation: {
-    async createPost(_, {body}, context) {
-      const user = checkAuth(context)
-      console.log(user)
+    async createPost(_, { body }, context) {
+      const user = checkAuth(context);
+      console.log(user);
       const newPost = new Post({
         body,
         user: user.id,
         username: user.username,
-        createdAt: new Date().toISOString()
-      })
+        createdAt: new Date().toISOString(),
+      });
 
-      const post = await newPost.save()
-      return post
+      const post = await newPost.save();
+
+      context.pubsub.publish('NEW_POST', {
+        newPost: post
+      })
+      return post;
     },
-    async deletePost(_, {postId}, context) {
-      const user = checkAuth(context)
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
 
       try {
-        const post = await Post.findById(postId)
-        if(user.username === post.username) {
-          await post.delete()
-          return 'Post deleted successfully'
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return "Post deleted successfully";
         } else {
-          throw new AuthenticationError('Action not allowed')
+          throw new AuthenticationError("Action not allowed");
         }
-      } catch(err) {
-        throw new Error(err)
+      } catch (err) {
+        throw new Error(err);
       }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          // Post already likes, unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+          await post.save();
+        } else {
+          // Not liked, like post
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError();
+      }
+    },
+  },
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST')
     }
   }
-}
+};
